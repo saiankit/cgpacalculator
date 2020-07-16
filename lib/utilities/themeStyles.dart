754301395eefe21d3f3e95706e8d51cfa8bc489f
saiankit <saiankit30@gmail.com>
2020-07-16 10:24:22 +0530

    Refactored code for Course Card

--- lib/utilities/themeStyles.dart
+++ lib/utilities/themeStyles.dart
@@ -6,4 +6,35 @@ class ThemeStyles {
     fontSize: 15.0,
     color: Colors.grey[600],
   );
+
+  static BoxDecoration courseCardDecoration = BoxDecoration(
+    border: Border.all(
+      color: Colors.grey,
+    ),
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(10.0),
+      bottomLeft: Radius.circular(10.0),
+      topRight: Radius.circular(30.0),
+      bottomRight: Radius.circular(30.0),
+    ),
+    color: Colors.white,
+  );
+
+  static BoxDecoration courseCardCourseInfo = BoxDecoration(
+    borderRadius: BorderRadius.circular(10.0),
+    color: Colors.black,
+  );
+
+  static BoxDecoration courseCardGradeInfo = BoxDecoration(
+    borderRadius: BorderRadius.circular(30.0),
+    color: Colors.black,
+  );
+
+  static BoxDecoration modalBottomSheetDecoration = BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.only(
+      topLeft: const Radius.circular(30.0),
+      topRight: const Radius.circular(30.0),
+    ),
+  );
 }
